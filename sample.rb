d503aa7b93b46bc388c1b75f303454a2ffca9aed
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-01-16 16:35:28 +0000

    Ruby課題シリーズ1 提出

---
+++ sample.rb
@@ -0,0 +1,48 @@
+class Player
+  def hand
+    puts "数字を入力してください。"
+    puts "0: グー"
+    puts "1: チョキ"
+    puts "2:  パー"
+    player_hand = gets.to_i
+  end
+end
+
+class Enemy
+  def hand
+    enemy_hand = rand(3)
+  end
+end
+
+class Janken
+  def pon(player_hand, enemy_hand)
+    jankens = ["グー", "チョキ","パー"]
+
+  if player_hand == enemy_hand
+    puts "相手の手は#{jankens[enemy_hand]}です"
+    return true
+     elsif(player_hand == 0 && enemy_hand == 1)||(player_hand == 1 && enemy_hand == 2)||(player_hand == 2 && enemy_hand == 0)
+    puts "相手の手は#{jankens[enemy_hand]}です"
+    puts "あなたの勝ちです"
+    return false
+
+  elsif(player_hand == 0 && enemy_hand == 2)||(player_hand == 1 && enemy_hand == 0)||(player_hand == 2 && enemy_hand == 1)
+    puts "相手の手は#{jankens[enemy_hand]}です"
+    puts "あなたの負けです"
+    return false
+  else player_hand <=3
+    puts "0~2の数字を入力してください"
+    return true
+    end
+  end
+end
+    
+player = Player.new
+enemy = Enemy.new
+janken = Janken.new
+
+next_game = true
+
+while next_game
+ next_game = janken.pon(player.hand, enemy.hand)
+end
