2a972d29a71bff09db857c568430f00607b76f25
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-01-20 03:39:59 +0000

    じゃんけん修正

--- sample.rb
+++ sample.rb
@@ -1,25 +1,31 @@
 class Player
   def hand
     puts "数字を入力してください。"
-    puts "0: グー"
-    puts "1: チョキ"
-    puts "2:  パー"
-    player_hand = gets.to_i
+    puts "0: グー\n"
+    puts "1: チョキ\n"
+    puts "2:  パー\n"
+    input_hand = gets
+    player_hand = Integer(input_hand)
   end
 end
 
 class Enemy
   def hand
     enemy_hand = rand(3)
+    
   end
 end
 
+
 class Janken
   def pon(player_hand, enemy_hand)
-    jankens = ["グー", "チョキ","パー"]
+    
 
+    jankens = ["グー", "チョキ","パー"]
+    
   if player_hand == enemy_hand
     puts "相手の手は#{jankens[enemy_hand]}です"
+    puts "あいこです"
     return true
      elsif(player_hand == 0 && enemy_hand == 1)||(player_hand == 1 && enemy_hand == 2)||(player_hand == 2 && enemy_hand == 0)
     puts "相手の手は#{jankens[enemy_hand]}です"
@@ -30,11 +36,11 @@ class Janken
     puts "相手の手は#{jankens[enemy_hand]}です"
     puts "あなたの負けです"
     return false
-  else player_hand <=3
+  else 
     puts "0~2の数字を入力してください"
     return true
     end
-  end
+    end
 end
     
 player = Player.new
