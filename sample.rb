165f3b11dfbbf9d327e704c483504433590ef0fe
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-01-20 09:41:10 +0000

    Ruby課題1 再修正

--- sample.rb
+++ sample.rb
@@ -1,11 +1,10 @@
 class Player
   def hand
     puts "数字を入力してください。"
-    puts "0: グー\n"
-    puts "1: チョキ\n"
-    puts "2:  パー\n"
-    input_hand = gets
-    player_hand = Integer(input_hand)
+    puts "1: グー\n"
+    puts "2: チョキ\n"
+    puts "3:  パー\n"
+    player_hand = gets.to_i
   end
 end
 
@@ -16,23 +15,22 @@ class Enemy
   end
 end
 
-
 class Janken
   def pon(player_hand, enemy_hand)
     
 
     jankens = ["グー", "チョキ","パー"]
     
-  if player_hand == enemy_hand
+  if (player_hand == 1 && enemy_hand == 0)||(player_hand == 2 && enemy_hand == 1)||(player_hand == 3 && enemy_hand == 2)
     puts "相手の手は#{jankens[enemy_hand]}です"
     puts "あいこです"
     return true
-     elsif(player_hand == 0 && enemy_hand == 1)||(player_hand == 1 && enemy_hand == 2)||(player_hand == 2 && enemy_hand == 0)
+     elsif(player_hand == 1 && enemy_hand == 1)||(player_hand == 2 && enemy_hand == 2)||(player_hand == 3 && enemy_hand == 0)
     puts "相手の手は#{jankens[enemy_hand]}です"
     puts "あなたの勝ちです"
     return false
 
-  elsif(player_hand == 0 && enemy_hand == 2)||(player_hand == 1 && enemy_hand == 0)||(player_hand == 2 && enemy_hand == 1)
+  elsif(player_hand == 1 && enemy_hand == 2)||(player_hand == 2 && enemy_hand == 0)||(player_hand == 3 && enemy_hand == 1)
     puts "相手の手は#{jankens[enemy_hand]}です"
     puts "あなたの負けです"
     return false
